14379515ff5563f2f77f87dae95b9aaa5f100834
Shelly <sia1940@gmail.com>
2025-05-06 16:34:14 +0800

    調整清洗露營場名稱跟地址的檔案 ; 刪除其他不用的舊檔

--- shelly_code/t_campground_clean.py
+++ shelly_code/t_campground_clean.py
@@ -4,107 +4,133 @@ from pathlib import Path
 import pandas as pd
 import re
 
-filepath = Path("C:\TJR101_03_Project\.venv\project\shelly_merge.csv")
-df = pd.read_csv(filepath, encoding="utf-8")
+def clean_campground_name(series: pd.Series):
+    """
+    處理google map爬下來的露營場名稱
+    """
+    # 用正規表達式合併為一個 pattern
+    split_symbols = [
+        r"｜", r"\|",   # 全形與半形的直線
+        r"－", r"-",   # 全形與半形的 dash
+        r"／", r"/",   # 全形與半形的 slash
+        r"、", r"‧",   # 中文列點符號
+        r"》", r"》",   # 全形右雙括號
+        r"】", r"\]",  # 全形右方括號
+        r"（",  # 括號
+        r"」", r"「", 
+        r"《", r"：",
+    ]
+    pattern = r"|".join(split_symbols)
 
-# 處理特定的露營場名稱
-df.loc[df["Campsite"].isna(), "Campsite"] = "《綠野雲仙》 露營區"
+    name_list = []
 
-# 用正規表達式合併為一個 pattern
-split_symbols = [
-    r"｜", r"\|",   # 全形與半形的直線
-    r"－", r"-",   # 全形與半形的 dash
-    r"／", r"/",   # 全形與半形的 slash
-    r"、", r"‧",   # 中文列點符號
-    r"》", r"》",   # 全形右雙括號
-    r"】", r"\]",  # 全形右方括號
-    r"（",  # 括號
-    r"」", r"「", 
-    r"《", r"：",
-]
-pattern = r"|".join(split_symbols)
+    for _, row in series.items():
+        if pd.isna(row):
+            name_list.append("")
+            continue
+            
+        row_str = str(row)
+        matches = list(re.finditer(pattern, row_str))
+        sharp_matches = list(re.finditer(r"#", row_str))
 
-name_list = []
+        if pd.isna(row):
+            name_list.append("")
+            continue
 
-for _, row in df["Campsite"].items():
-    row_str = str(row)
-    matches = list(re.finditer(pattern, row_str))
-    sharp_matches = list(re.finditer(r"#", row_str))
+        # 檢查是否有兩個以上的"#""   
+        elif len(sharp_matches) >= 2:
+            second_sharp = sharp_matches[1]
+            name_list.append(row_str[:second_sharp.start()].strip())
 
-    if pd.isna(row):
-        name_list.append("")
-        continue
-
-    # 檢查是否有兩個以上的"#""   
-    elif len(sharp_matches) >= 2:
-        second_sharp = sharp_matches[1]
-        name_list.append(row_str[:second_sharp.start()].strip())
-
-    elif matches:
-        match = matches[0]
-        # 特殊符號的起始位置>=3才切，避開少數12號露營地等狀況
-        if match.start() >= 3:
-            name_list.append(row_str[:match.start()].strip())
+        elif matches:
+            match = matches[0]
+            # 特殊符號的起始位置>=3才切，避開少數12號露營地等狀況
+            if match.start() >= 3:
+                name_list.append(row_str[:match.start()].strip())
+            else:
+                name_list.append(row_str.strip().replace("「", ""))
         else:
-            name_list.append(row_str.strip().replace("「", ""))
-    else:
-        name_list.append(row_str.strip())
+            name_list.append(row_str.strip())
 
-df["Campsite"] = name_list
+    return pd.Series(name_list)
 
-# 處理地址
-cleaned_address = []
-for _, row in df["Address"].items():
-    # 清除開頭郵遞區號
-    row = re.sub(r"^\d{3,6}\s*", "", str(row)).replace(" ", "")
 
-    # 清除其他雜字
-    row = row.replace("、", "").replace("No.", "").replace("No", "").replace("TW", "")
+def clean_address(series: pd.Series):
+    """
+    處理google map的地址
+    """
+    cleaned_address = []
+    for _, row in series.items():
+        # 清除開頭郵遞區號
+        row = re.sub(r"^\d{3,6}\s*", "", str(row)).replace(" ", "")
+
+        # 清除其他雜字
+        row = row.replace("、", "").replace("No.", "").replace("No", "").replace("TW", "").replace("。", "")
+
+        # 先抓出最後的地址號碼
+        match_house_number = re.search(r"\d{1,3}(-\d{1,3})?(\d{1,3})?(號|之\d+)號", row)
+        house_number = ""
+        if match_house_number:
+            house_number = match_house_number.group()
+            row = re.sub(re.escape(house_number), "", row)
+
+        # 清除座標
+        row = re.sub(r"\d{2,3}(?:\.\d+)+", "", row) #小數點座標
+        row = re.sub(r"\d{1,3}°\d{1,3}'\d{0,3}(\.\d+)?[NSEW]?", "", row)  # 度分秒座標
+        row = re.sub(r"\d{1,3}°\d{1,3}'(\.\d+)?[NSEW]?", "", row)        # 度分座標
+        row = re.sub(r"\d+[°\'\"″]{1,2}[NSEW]", "", row)
+        row = row.replace("°", "") # 清除可能漏掉的°
+
+        # 清理地址尾巴的郵遞區號
+        row = re.sub(r"\d{1,6}$", "", str(row)).strip()
+        
+        # 清除縣市區後面接郵遞區號
+        row = re.sub(r"([區鄉鎮])\d{3,5}", r"\1", row)
+
+        # 寫回格式正常的地址
+        if len(row) <= 2 or (row[2] in ["縣", "市", "鄉"]):
+            # 加回門牌
+            if house_number and house_number not in row:
+                row += house_number
+            cleaned_address.append(row)
+            continue 
+
+        # 開始清理地址倒反放的狀況
+        keywords = ["縣", "市", "區", "段", "鄉", "鎮", "鄰", "鄉道", "路", "巷", "街", "號", "樓", "UnnamedRd", "UnnamedRoad", "BaishiChi", "村民石"]
+
+        address_clean = row
+        for key in keywords:
+            address_clean = re.sub(f"({key})", r"\1 ", address_clean)
+
+        address_parts = address_clean.split()
+        t_address = " ".join(address_parts[::-1]).replace(" ", "")
+
+        # 反轉完成後，再檢查「號 + 路街巷」是否顛倒 → 修正
+        t_address = re.sub(r"(\d{1,4})([^\d之號]{1,5}[路街巷])", r"\2\1號", t_address)
+
+        # 加回門牌
+        if house_number and house_number not in t_address:
+            t_address += house_number
+        
+        cleaned_address.append(t_address)
+
+    return pd.Series(cleaned_address)
+
+def main():
+    filepath = Path("C:\TJR101_03_Project\.venv\project\shelly_merge.csv")
+    df = pd.read_csv(filepath, encoding="utf-8")
+
+    # 處理特定的露營場名稱
+    df.loc[df["Campsite"].isna(), "Campsite"] = "《綠野雲仙》 露營區"
     
-    # 清除座標
-    row = re.sub(r"\d{2,3}(?:\.\d+)+", "", row) #小數點座標
-    row = re.sub(r"\d{1,3}°\d{1,3}'\d{0,3}(\.\d+)?[NSEW]?", "", row)  # 度分秒座標
-    row = re.sub(r"\d{1,3}°\d{1,3}'(\.\d+)?[NSEW]?", "", row)        # 度分座標
-    row = re.sub(r"\d+[°\'\"″]{1,2}[NSEW]", "", row)
-    row = row.replace("°", "") # 清除可能不會清掉的°
+    df["Campsite"] = clean_campground_name(df["Campsite"])
+    df["Address"] = clean_address(df["Address"])
+    dir_path = Path(".venv", "project")
+    dir_path.mkdir(parents=True, exist_ok=True)
+    df.to_csv(dir_path / "merge_v3.csv", index=False)
 
-    # 清理地址尾巴的郵遞區號
-    row = re.sub(r"\d{1,6}$", "", str(row)).strip()
+    # test_address = pd.Series(["233新北市烏來區西羅岸路132之5號"])
+    # result = clean_address(test_address)
+    # print(result)
 
-    # 清除縣市區後面接郵遞區號
-    row = re.sub(r"([區鄉鎮])\d{3,5}", r"\1", row)
-
-    if len(row) <= 2 or (len(row) >= 3 and row[2] in ["縣", "市", "鄉"]):
-        cleaned_address.append(row)
-        continue 
-
-    # 開始清理地址倒反放的狀況
-    keywords = ["縣", "市", "區", "段", "鄉", "鎮", "鄰", "鄉道", "路", "巷", "街", "號", "UnnamedRd", "UnnamedRoad", "BaishiChi", "村民石"]
-
-    # 特殊處理：數字放在xx路街巷鄉道前 → 轉為 xx路街巷鄉道 + 數字 + 號
-    row = re.sub(r"(\d+)([^\d\s]{1,10}[路街巷鄉道])", r"\2\1號", row)
-
-    # 抓出門牌號碼（放最後用）
-    match_house_number = re.search(r"\d+(-\d+)?(號|之\d+)", row)
-    house_number = ""
-    if match_house_number:
-        house_number = match_house_number.group()
-        row = re.sub(re.escape(house_number), "", row)
-
-    address_clean = row
-    for key in keywords:
-        address_clean = re.sub(f"({key})", r"\1 ", address_clean)
-
-    address_parts = address_clean.split()
-    t_address = " ".join(address_parts[::-1]).replace(" ", "")
-
-    # 加回門牌
-    if house_number and house_number not in t_address:
-        t_address += house_number
-
-    print(t_address)
-    cleaned_address.append(t_address)
-
-df["Address"] = cleaned_address
-
-df.to_csv("merge_v2", index=False)
+main()
