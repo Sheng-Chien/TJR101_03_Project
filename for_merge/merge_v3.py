6b633a682cf3afa89c047c3434a134dbdf1a97ad
Eta <yitaoshieh@gmail.com>
2025-05-08 16:05:56 +0800

    營區對照表新進度, 正規化serry表格欄位 營地名稱簡化後搭配地址比對, 可以正確區分出90%資料, 下次進度: 由地址相似度60~80間做詳細區分, 解決剩下10%分類問題

--- for_merge/merge_v3.py
+++ for_merge/merge_v3.py
@@ -1,9 +1,6 @@
-import jieba
 from rapidfuzz import fuzz
 import pandas as pd
 from pathlib import Path
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from sqlalchemy import create_engine
 import re
 
@@ -36,21 +33,6 @@ def uploadMYSQL(df:pd.DataFrame, table:str):
     df.to_sql(name=table, con=engine, if_exists='replace', index=False)
 
 
-
-# pattern = r"([\u4e00-\u9fa5]{2,}(縣|市)[\u4e00-\u9fa5]{1,}(鄉|鎮|市|區|村)(?:[\u4e00-\u9fa5-\d]{0,}(?:段|路|巷|弄|號))*)"
-def addressNormalize(df:pd.Series):
-
-    # df.replace("")
-
-    text = "南投縣鹿谷鄉鳳凰村鳳園路88-50號"
-    # 地址必定以XX 市/縣 開頭
-    # *? 非貪婪模式，會停在 XX縣市的位置
-    pattern = r".*?([\u4e00-\u9fa5]{2}(縣|市))"
-    # 使用 re.sub 移除前面的內容，保留從縣市開始的部分
-    match = re.search(pattern, text)
-
-    print(match)
-
 def findTopSimilar(s:pd.Series):
     """找出和自己最相似的元素, 並以同等長度的series回傳該元素的所在index"""
     similar_idx = []
@@ -75,17 +57,56 @@ def idxToCol(s:pd.Series, idx:pd.Series):
     """把索引轉換成索引對應的元素名稱"""
     return s.loc[idx].values
     
+def campsiteNF(text:str):
+    # 去除不可見字元以及特殊符號
+    text = re.sub(r'[\s\u200b\u3000]', '', text)                      # 刪除不可見字元
+    text = re.sub(r'[^\u4e00-\u9fa5a-zA-Z0-9]', '', text)            # 刪除特殊符號
 
+    # 要去除的字典
+    dictionary = ["露營地", "露營區", "營地", "營區", 
+                  "農場", "民宿", "休閒", "溫泉渡假村", 
+                  "露營", "園區", "農莊", "莊園",
+                  "農村", "農園", "農庄", "農業"]
+    # 重新排序，避免子集合先被刪除(營區&露營區)
+    dictionary.sort(key=len, reverse=True)
+
+    pattern = '|'.join(map(re.escape, dictionary))  # 安全建立 regex
+    text = re.sub(pattern, "", text)
+    return text
+
+def addressNF(text:str):
+    # 將門牌正規為阿拉伯數字
+    text = re.sub(r'(\d+)-(\d+)', r'\1之\2', text)
+    # 繁簡統一
+    text = text.replace("台","臺")
+    
+    return text
+
+
+def addressRatio(addr1:str, addr2:str):
+    # 提取縣市名
+    conty1 = re.search(r'([\u4e00-\u9fa5]{2}(縣|市))', addr1)
+    conty2 = re.search(r'([\u4e00-\u9fa5]{2}(縣|市))', addr2)
+    
+    # 如果縣市名不同直接出局
+    # 沒有縣市的通常是沒有縣(新竹), 而且名稱很短, 直接進下一輪
+    if conty1 and conty2 and conty1.group(0) != conty2.group(0):
+        return 0
+    
+    score =  fuzz.partial_ratio(addr1, addr2)
+
+    return score
 
 def main():
     # 獲取所有
     df_base = getAllCSVtoDF()
 
     # 分別把 "營區名稱" 以及 "營區地址" 提取出來作正規化及簡化
-    s_site = df_base["Campsite"]
-    s_address = df_base["Address"]
+    s_site = df_base["Campsite"].map(campsiteNF)
+    s_address = df_base["Address"].map(addressNF)
 
     # 取得最相似名稱的對應索引
+    print("比較相似度")
     s_similar_idx = findTopSimilar(s_site)
 
     # 整理資料，不需要的欄位可以註解掉
@@ -97,19 +118,19 @@ def main():
     # 相似營地名稱
     df_base["similar_site"] = idxToCol(s_site, s_similar_idx)
     # 名稱相似度分數
-    df_base["site_ratio"] = df_base["site_NF"].combine(df_base["similar_site"], lambda x, y: fuzz.partial_ratio(x,y))
+    df_base["site_ratio"] = df_base["site_NF"].combine(df_base["similar_site"], lambda x, y: fuzz.partial_ratio(x,y)).round(2)
     
     
     # 簡化營區地址
     df_base["address_NF"] = s_address
     # 相似營地地址
     df_base["similar_address"] = idxToCol(s_address, s_similar_idx)
-    # 相似營區分數
-    df_base["address_ratio"] = df_base["address_NF"].combine(df_base["similar_address"], lambda x, y: fuzz.partial_ratio(x,y))
+    # 相似營區地址分數
+    df_base["address_ratio"] = df_base["address_NF"].combine(df_base["similar_address"], lambda x, y: addressRatio(x,y)).round(2)
     
     save_path = Path(__file__).parent/"results/results.csv"
     saveFile(df_base, save_path)
-    uploadMYSQL(df_base, "mergev3")
+    uploadMYSQL(df_base.reset_index(names="idx"), "mergev3")
 
 
     
